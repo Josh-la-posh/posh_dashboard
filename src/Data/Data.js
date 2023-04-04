29258928460f6815a5701e47ef80b28c0e81af8b
Joshua <joshuamayowa23@yahoo.com>
2023-04-04 07:03:13 +0100

    dashboard

--- src/Data/Data.js
+++ src/Data/Data.js
@@ -18,7 +18,7 @@ export const SidebarData = [
 ];
 
 export const CardsData = [
-  {title: 'Sales', color: {backGround: 'linear-gradient(180deg, #bb67ff 0%, #c484f3 100%)', boxShadow: '0px 10px 20px 0px #e0c6f5'}, barValue: 70, value: '25,970', png: UilUsdSquare, series: {name: 'Sales', data: [31, 40, 28, 51, 109, 100]}},
-  {title: 'Sales', color: {backGround: 'linear-gradient(180deg, #ff919d 0%, #fc929d 100%)', boxShadow: '0px 10px 20px 0px #fdc0c7'}, barValue: 80, value: '14,270', png: UilMoneyWithdrawal, series: {name: 'Revenue', data: [10, 100, 50, 51, 70, 80, 30, 40]}},
-  {title: 'Sales', color: {backGround: 'linear-gradient(rgb(248, 212, 154) -146.42%, rgb(255, 202, 113) -46.42%)', boxShadow: '0px 10px 20px 0px #f9d59b'}, barValue: 60, value: '4,270', png: UilClipboardAlt, series: {name: 'Expenses', data: [10, 25, 15, 30, 12, 15, 20]}},
+  {title: 'Sales', color: {backGround: 'linear-gradient(180deg, #bb67ff 0%, #c484f3 100%)', boxShadow: '0px 10px 20px 0px #e0c6f5'}, barValue: 70, value: '25,970', png: UilUsdSquare, series: [{name: 'Sales', data: [31, 40, 28, 51, 109, 100]}]},
+  {title: 'Sales', color: {backGround: 'linear-gradient(180deg, #ff919d 0%, #fc929d 100%)', boxShadow: '0px 10px 20px 0px #fdc0c7'}, barValue: 80, value: '14,270', png: UilMoneyWithdrawal, series: [{name: 'Revenue', data: [10, 100, 50, 51, 70, 80, 30, 40]}]},
+  {title: 'Sales', color: {backGround: 'linear-gradient(rgb(248, 212, 154) -146.42%, rgb(255, 202, 113) -46.42%)', boxShadow: '0px 10px 20px 0px #f9d59b'}, barValue: 60, value: '4,270', png: UilClipboardAlt, series: [{name: 'Expenses', data: [10, 25, 15, 30, 12, 15, 20]}]},
 ]
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,38 +1,127 @@
 import { useState } from "react";
-import { AnimateSharedLayout } from "framer-motion";
+import { motion, AnimateSharedLayout } from "framer-motion";
 import "./Card.css";
 import { CircularProgressbar } from "react-circular-progressbar";
-import 'react-circular-progressbar/dist/styles.css';
+import "react-circular-progressbar/dist/styles.css";
+import Chart from "react-apexcharts";
+import { UilTimes } from "@iconscout/react-unicons";
 
 const Card = (props) => {
   const [expanded, setExpanded] = useState(false);
   return (
     <AnimateSharedLayout>
-      {expanded ? "ExpandedCard" : <CompactCard param={props} />}
+      {expanded ? (
+        <ExpandedCard param={props} setExpanded={() => setExpanded(false)} />
+      ) : (
+        <CompactCard param={props} setExpanded={setExpanded} />
+      )}
     </AnimateSharedLayout>
   );
 };
 
 // COMPACT CARD
 
-function CompactCard({ param }) {
+function CompactCard({ param, setExpanded }) {
   const Png = param.png;
   return (
-    <div className="CompactCard"
-        style={{background: param.color.backGround, boxShadow: param.color.boxShadow}}
+    <motion.div
+      className="CompactCard"
+      style={{
+        background: param.color.backGround,
+        boxShadow: param.color.boxShadow,
+      }}
+      onClick={() => setExpanded(true)}
+      layoutId="expandableCard"
     >
       <div className="radialBar">
-          <CircularProgressbar 
-            value={param.barValue}
-            text={`${param.barValue}%`}
-          />
+        <CircularProgressbar
+          value={param.barValue}
+          text={`${param.barValue}%`}
+        />
+        <span>{param.title}</span>
       </div>
       <div className="detail">
         <Png />
         <span>${param.value}</span>
         <span>Last 24 hours</span>
       </div>
-    </div>
+    </motion.div>
+  );
+}
+
+// EXPANDED CARD
+
+function ExpandedCard({ param, setExpanded }) {
+  const data = {
+    options: {
+      chart: {
+        type: "area",
+        height: "auto",
+      },
+      dropShadow: {
+        enable: false,
+        enableOnSeries: undefined,
+        top: 0,
+        left: 0,
+        blur: 3,
+        color: "#000",
+        opacity: 0.35,
+      },
+      fill: {
+        colors: ["#fff"],
+        type: "gradient",
+      },
+      dataLabels: {
+        enable: false,
+      },
+      stroke: {
+        curve: "smooth",
+        colors: ["white"],
+      },
+      tooltip: {
+        x: {
+          format: "dd/MM/yy HH:mm",
+        },
+      },
+      grid: {
+        show: true,
+      },
+      xaxis: {
+        type: "datetime",
+        categories: [
+          "2018-09-19T00:00:00.000Z",
+          "2018-09-19T01:30:00.000Z",
+          "2018-09-19T02:30:00.000Z",
+          "2018-09-19T03:30:00.000Z",
+          "2018-09-19T04:30:00.000Z",
+          "2018-09-19T05:30:00.000Z",
+          "2018-09-19T06:30:00.000Z",
+        ],
+      },
+    },
+  };
+  return (
+    <motion.div
+      className="ExpandedCard"
+      style={{
+        background: param.color.backGround,
+        boxShadow: param.color.boxShadow,
+      }}
+      layoutId="expandableCard"
+    >
+      <div style={{alignSelf:'flex-end', cursor: 'pointer', color: '#fff'}}>
+        <UilTimes onClick={setExpanded} />
+      </div>
+      <span>{param.title}</span>
+      <div className="chartContainer">
+        <Chart
+          options={data.options}
+          series={param.series}
+          type="area"
+        />
+      </div>
+      <span>Last 24 hours</span>
+    </motion.div>
   );
 }
 
